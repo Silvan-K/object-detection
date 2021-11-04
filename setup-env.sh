0039fe47649365f62fc730ace0917b96f3be8ab4
Silvan Kuttimalai <silvan_kuttimalai@gmx.de>
2021-11-04 16:23:22 -0700

    Put opencv downgrade into setup-env.sh

--- setup-env.sh
+++ setup-env.sh
@@ -11,3 +11,6 @@ export PYTHONPATH=$PYTHONPATH:$(git rev-parse --show-toplevel)/yolov5/
 
 # Install dependencies
 pip install -r $(git rev-parse --show-toplevel)/yolov5/requirements.txt
+
+# Bug in opencv, need to downgrade
+pip install --upgrade opencv-python==4.3.0.38
